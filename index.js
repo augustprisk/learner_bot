3a58c84199f9988500d8318c2085e4b0e1fb785b
augustprisk <august.prisk@fluxfederation.com>
2025-12-27 16:13:30 +1300

    Learner bot project setup

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(process.env.A);
+console.log(process.env.B + 9); // 1239 (this concatenates the number to the string!)
+console.log(Number(process.env.C) + 9); // 132
+console.log(process.env.DISCORD_TOKEN);
